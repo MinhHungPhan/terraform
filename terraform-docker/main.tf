6f8586346ecec68306fe7e72858d2402714ffa65
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-04-24 15:24:25 +0000

    added multiple resources and count

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,20 +14,17 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
+  count = 2
   length  = 4
   special = false
   upper   = false
 }
 
-resource "random_string" "random2" {
-  length  = 4
-  special = false
-  upper   = false
-}
 
 
 resource "docker_container" "nodered_container" {
-  name  = join("-",["nodered",random_string.random.result])
+  count = 2
+  name  = join("-",["nodered",random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
@@ -35,31 +32,23 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-resource "docker_container" "nodered_container2" {
-  name  = join("-",["nodered",random_string.random2.result])
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-    # external = 1880
-  }
-}
 
 output "IP-Address" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "The IP address and external port of the container"
 }
 
 output "Container-name" {
-  value       = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[0].name
   description = "The name of the container"
 }
 
 output "IP-Address2" {
-  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
   description = "The IP address and external port of the container"
 }
 
 output "Container-name2" {
-  value       = docker_container.nodered_container2.name
+  value       = docker_container.nodered_container[1].name
   description = "The name of the container"
 }
