210fa8659707674771449392dc23f52c8e301198
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-04 20:57:07 +0000

    added terraform import

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -39,6 +39,6 @@ output "container-name" {
 }
 
 output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],)]
+  value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],i.ports[*]["external"])]
   description = "The IP address and external port of the container"
 }
