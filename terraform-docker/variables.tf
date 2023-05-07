f817dc433bcf9221c0a0a59180c5c3730e8ab9a2
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-07 14:22:11 +0000

    added sensitive flag to external port variable

--- terraform-docker/variables.tf
+++ terraform-docker/variables.tf
@@ -1,18 +1,19 @@
 variable "int_port" {
-  type    = number
-  default = 1880
-  
-  validation{
-    condition = var.int_port == 1880
+  type      = number
+  default   = 1880
+
+  validation {
+    condition     = var.int_port == 1880
     error_message = "The internal port must be 1880."
   }
 }
 
 variable "ext_port" {
-  type    = number
-  
-   validation{
-    condition = var.ext_port <= 65535 && var.ext_port > 0
+  type = number
+  sensitive = true
+
+  validation {
+    condition     = var.ext_port <= 65535 && var.ext_port > 0
     error_message = "The external port must be in the valid range 0 - 65535."
   }
 }
